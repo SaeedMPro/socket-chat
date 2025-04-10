08953a4db63156dd1799bb12510550caa81f1188
saeedmpro <mzahry36@gmail.com>
2025-04-10 17:20:44 +0330

    Chore: Enhance file transfer capabilities and improve connection handling with mutex for thread safety

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/SaeedMPro/socket-chat/config"
@@ -15,6 +20,7 @@ import (
 var (
 	clientConfig model.Config
 	currentConn  net.Conn
+	connMux      sync.Mutex
 )
 
 func init() {
@@ -56,9 +62,8 @@ func startListener(self model.Client) {
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		handleNewConnection(connection)
+		go handleNewConnection(connection)
 	}
-
 }
 
 func connectToPeer(peer model.Client) {
@@ -71,12 +76,15 @@ func connectToPeer(peer model.Client) {
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		handleNewConnection(connection)
+		go handleNewConnection(connection)
 		break
 	}
 }
 
 func handleNewConnection(conn net.Conn) {
+	connMux.Lock()
+	defer connMux.Unlock()
+
 	if currentConn != nil {
 		currentConn.Close()
 	}
@@ -92,6 +100,7 @@ func handleNewConnection(conn net.Conn) {
 }
 
 func receiveMessages(conn net.Conn, done chan struct{}) {
+	defer conn.Close()
 	reader := bufio.NewReader(conn)
 
 	for {
@@ -100,13 +109,45 @@ func receiveMessages(conn net.Conn, done chan struct{}) {
 			fmt.Println("\nConnection lost")
 			break
 		}
-		fmt.Printf("\n\t\t\t\t\t\t\t\t %s ", msg)
+		msg = strings.TrimSpace(msg)
+
+		if strings.HasPrefix(msg, "FILE ") {
+			parts := strings.SplitN(msg, " ", 3)
+			if len(parts) != 3 {
+				fmt.Println("\nInvalid file header")
+				continue
+			}
+
+			filename := parts[1]
+			size, err := strconv.Atoi(parts[2])
+			if err != nil {
+				fmt.Println("\nInvalid file size:", parts[2])
+				continue
+			}
+
+			data := make([]byte, size)
+			_, err = io.ReadFull(reader, data)
+			if err != nil {
+				fmt.Printf("\nError receiving file: %v\n", err)
+				continue
+			}
+
+			err = os.WriteFile(filename, data, 0644)
+			if err != nil {
+				fmt.Printf("\nError saving file: %v\n", err)
+			} else {
+				fmt.Printf("\nFile received: %s (%d bytes)\n", filename, size)
+			}
+		} else {
+			fmt.Printf("\n\t\t\t\t\t\t\t\t %s\n", msg)
+		}
 	}
 
 	close(done)
 }
 
 func sendMessages(conn net.Conn, done chan struct{}) {
+	defer conn.Close()
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -118,12 +159,42 @@ func sendMessages(conn net.Conn, done chan struct{}) {
 			if !scanner.Scan() {
 				return
 			}
-			msg := scanner.Text()
+			input := scanner.Text()
 
-			_, err := fmt.Fprintln(conn, msg)
-			if err != nil {
-				fmt.Println("Error sending message:", err)
-				return
+			if strings.HasPrefix(input, "/sendfile ") {
+				parts := strings.SplitN(input, " ", 2)
+				if len(parts) < 2 {
+					fmt.Println("Usage: /sendfile <filename>")
+					continue
+				}
+
+				filename := parts[1]
+				data, err := os.ReadFile(filename)
+				if err != nil {
+					fmt.Printf("Error reading file: %v\n", err)
+					continue
+				}
+
+				header := fmt.Sprintf("FILE %s %d\n", filepath.Base(filename), len(data))
+				_, err = fmt.Fprint(conn, header)
+				if err != nil {
+					fmt.Printf("Error sending file header: %v\n", err)
+					return
+				}
+
+				_, err = conn.Write(data)
+				if err != nil {
+					fmt.Printf("Error sending file data: %v\n", err)
+					return
+				}
+
+				fmt.Printf("File '%s' sent successfully!\n", filename)
+			} else {
+				_, err := fmt.Fprintln(conn, input)
+				if err != nil {
+					fmt.Println("Error sending message:", err)
+					return
+				}
 			}
 		}
 	}
